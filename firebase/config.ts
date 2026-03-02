675090aae72d1c7c5ebdfa2b6fcc98e9417ab2ba
Shahwaiz Ali <116798948+shahwaiz-9@users.noreply.github.com>
2026-03-02 09:16:30 +0500

    Update Firebase config with placeholder values
    Replaced sensitive Firebase configuration values with placeholders.

--- firebase/config.ts
+++ firebase/config.ts
@@ -2,13 +2,13 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCwWzoVV6cxhY7ePb4jwAHcAIylgXdxNEQ",
-  authDomain: "diabetes-predictor-ba465.firebaseapp.com",
-  projectId: "diabetes-predictor-ba465",
-  storageBucket: "diabetes-predictor-ba465.firebasestorage.app",
-  messagingSenderId: "324935304014",
-  appId: "1:324935304014:web:beb5efa19fa956f9b7541f",
-  measurementId: "G-EJ18YFEQLL",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN_KEY",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "-",
+  appId: "-",
+  measurementId: "-",
 };
 
 const app = initializeApp(firebaseConfig);
